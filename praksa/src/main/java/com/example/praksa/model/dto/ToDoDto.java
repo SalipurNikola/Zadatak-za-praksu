c36a3d1a20c7425429d646f9dddd773d17d327ae
SalipurNikola <salipur.in16.2019@uns.ac.rs>
2024-03-05 21:07:47 +0100

    Create ToDoDto.java

---
+++ praksa/src/main/java/com/example/praksa/model/dto/ToDoDto.java
@@ -0,0 +1,70 @@
+package com.example.praksa.model.dto;
+
+import com.example.praksa.model.Priority;
+import jakarta.persistence.Column;
+
+public class ToDoDto {
+    private Long id;
+    private String naslov;
+    private String opis;
+    private Priority priority;
+    private Boolean completed;
+
+    public ToDoDto() {
+    }
+
+    public ToDoDto(Long id, String naslov, String opis, Priority priority, Boolean completed) {
+        this.id = id;
+        this.naslov = naslov;
+        this.opis = opis;
+        this.priority = priority;
+        this.completed = completed;
+    }
+
+    public ToDoDto(String naslov, String opis, Priority priority, Boolean completed) {
+        this.naslov = naslov;
+        this.opis = opis;
+        this.priority = priority;
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNaslov() {
+        return naslov;
+    }
+
+    public void setNaslov(String naslov) {
+        this.naslov = naslov;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+}

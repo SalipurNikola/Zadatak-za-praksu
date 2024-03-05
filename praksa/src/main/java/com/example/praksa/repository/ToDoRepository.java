a20193c5b8cd2049239e71b5a7e195dc5687a614
SalipurNikola <salipur.in16.2019@uns.ac.rs>
2024-03-05 20:04:38 +0100

    Create ToDoRepository.java

---
+++ praksa/src/main/java/com/example/praksa/repository/ToDoRepository.java
@@ -0,0 +1,11 @@
+package com.example.praksa.repository;
+
+import com.example.praksa.model.Korisnik;
+import com.example.praksa.model.ToDo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ToDoRepository extends JpaRepository<ToDo, Long> {
+    List<ToDo> findAllByKorisnik (Korisnik korisnik);
+}

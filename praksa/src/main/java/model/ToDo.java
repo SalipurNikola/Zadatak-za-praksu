4ff9d853eec785ac7c2648f0d0872124a35fcbca
SalipurNikola <salipur.in16.2019@uns.ac.rs>
2024-03-05 19:27:52 +0100

    Update ToDo.java

--- praksa/src/main/java/model/ToDo.java
+++ praksa/src/main/java/model/ToDo.java
@@ -1,7 +1,85 @@
 package model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
 public class ToDo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String naslov;
+
+    @Column
+    private String opis;
+
+    @Column
+    private Priority priority;
+
+    @Column
+    private Boolean completed;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Korisnik korisnik;
+
+    public ToDo() {
+    }
+
+    public ToDo(String naslov, String opis, Priority priority, Boolean completed, Korisnik korisnik) {
+        this.naslov = naslov;
+        this.opis = opis;
+        this.priority = priority;
+        this.completed = false;
+        this.korisnik = korisnik;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNaslov() {
+        return naslov;
+    }
+
+    public void setNaslov(String naslov) {
+        this.naslov = naslov;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    public Korisnik getKorisnik() {
+        return korisnik;
+    }
+
+    public void setKorisnik(Korisnik korisnik) {
+        this.korisnik = korisnik;
+    }
 }

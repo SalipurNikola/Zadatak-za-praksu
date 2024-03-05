95fa655de09b8e728d1be520a9fd0933b9b4657a
SalipurNikola <salipur.in16.2019@uns.ac.rs>
2024-03-05 19:11:53 +0100

    Create Korisnik.java

---
+++ praksa/src/main/java/model/Korisnik.java
@@ -0,0 +1,100 @@
+package model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Korisnik
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "kor_ime")
+    private String korisnickoIme;
+
+    @Column
+    private String lozinka;
+
+    @Column
+    private String ime;
+
+    @Column
+    private String prezime;
+
+    @Column
+    private LocalDate datumRodjenja;
+
+    @OneToMany(mappedBy = "kor_ime", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<ToDo>toDoSet = new HashSet<>();
+
+    public Korisnik() {
+    }
+
+    public Korisnik(String korisnickoIme, String lozinka, String ime, String prezime, LocalDate datumRodjenja) {
+        this.korisnickoIme = korisnickoIme;
+        this.lozinka = lozinka;
+        this.ime = ime;
+        this.prezime = prezime;
+        this.datumRodjenja = datumRodjenja;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getKorisnickoIme() {
+        return korisnickoIme;
+    }
+
+    public void setKorisnickoIme(String korisnickoIme) {
+        this.korisnickoIme = korisnickoIme;
+    }
+
+    public String getLozinka() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka = lozinka;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public LocalDate getDatumRodjenja() {
+        return datumRodjenja;
+    }
+
+    public void setDatumRodjenja(LocalDate datumRodjenja) {
+        this.datumRodjenja = datumRodjenja;
+    }
+
+    public Set<ToDo> getToDoSet() {
+        return toDoSet;
+    }
+
+    public void setToDoSet(Set<ToDo> toDoSet) {
+        this.toDoSet = toDoSet;
+    }
+}

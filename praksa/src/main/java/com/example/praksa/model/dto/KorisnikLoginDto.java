cfb2c573c4849017c6497197f600e53d1da7f7d2
SalipurNikola <salipur.in16.2019@uns.ac.rs>
2024-03-05 21:03:36 +0100

    Create KorisnikLoginDto.java

---
+++ praksa/src/main/java/com/example/praksa/model/dto/KorisnikLoginDto.java
@@ -0,0 +1,45 @@
+package com.example.praksa.model.dto;
+
+public class KorisnikLoginDto {
+    private Long id;
+    private String korisnickoIme;
+    private String lozinka;
+
+    public KorisnikLoginDto() {
+    }
+
+    public KorisnikLoginDto(Long id, String korisnickoIme, String lozinka) {
+        this.id = id;
+        this.korisnickoIme = korisnickoIme;
+        this.lozinka = lozinka;
+    }
+
+    public KorisnikLoginDto(String korisnickoIme, String lozinka) {
+        this.korisnickoIme = korisnickoIme;
+        this.lozinka = lozinka;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getKorisnickoIme() {
+        return korisnickoIme;
+    }
+
+    public void setKorisnickoIme(String korisnickoIme) {
+        this.korisnickoIme = korisnickoIme;
+    }
+
+    public String getLozinka() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka = lozinka;
+    }
+}

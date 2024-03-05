f4a0bf968c9ffc74b4ffcfa11167dd27b8de34f6
SalipurNikola <salipur.in16.2019@uns.ac.rs>
2024-03-05 19:50:24 +0100

    Create KorisnikRepository.java

---
+++ praksa/src/main/java/com/example/praksa/repository/KorisnikRepository.java
@@ -0,0 +1,10 @@
+package com.example.praksa.repository;
+
+import com.example.praksa.model.Korisnik;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KorisnikRepository extends JpaRepository<Korisnik, Long> {
+    Korisnik findAllByKorisnickoImeAndLozinka (String korisnickoIme, String lozinka);
+
+    Korisnik findAllByKorisnickoIme (String korisnickoIme);
+}
